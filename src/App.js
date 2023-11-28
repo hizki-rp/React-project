a5a1da418a896c26ddd9513cd5a2f7ff2b245d4e
hizki-rp <hizkielas@gmail.com>
2023-11-28 16:17:20 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';   
 
 import NavBar from './Components/NavBar';
+import Header from './Components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-   
-      <NavBar/>
-   
+     
+      <>
+         <NavBar/>
+         <Header/>
+      </>
+          
   );
 }
 
---
+++ src/Components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import tlogo1 from '../assets/javascript.png';
+import tlogo2 from '../assets/react.png';
+import tlogo3 from '../assets/bootstrap.png';
+import tlogo4 from '../assets/github.png';
+import bg from '../assets/download (2).jpeg';
+
+
+
+const Header = () => {
+  return (
+  
+    <div className='container container-header'>
+      
+        <div className='row'>
+            <div className='col'>
+                <h1>Tech Stacks</h1>
+                <div className="tech-logos">
+                 <img src={tlogo1}></img>
+                 <img src={tlogo2}></img>
+                 <img src={tlogo3}></img>
+                 <img src={tlogo4}></img>
+                </div>
+                <div>
+                  
+                </div>
+            </div>
+            <div className='col'>
+                <h1>Front-End Developer</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header;
