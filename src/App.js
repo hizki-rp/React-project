5855ead88d642116def86c078efc58b7772e58f3
hizki-rp <hizkielas@gmail.com>
2023-11-28 16:40:16 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';   
 
 import NavBar from './Components/NavBar';
+import Header from './Components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
    
+      <>
       <NavBar/>
+      <Header/>
+      </>
    
   );
 }
