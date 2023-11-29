343e3fc8e32f66c6b4832424abeec65c7eb42250
hizki-rp <hizkielas@gmail.com>
2023-11-29 20:00:59 +0300

    react typed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,6 +5,7 @@ import tlogo3 from '../assets/bootstrap.png';
 import tlogo4 from '../assets/github.png';
 import bg from '../assets/dfree.jpeg';
 
+import Typed from 'react-typed';
 
 
 const Header = () => {
@@ -13,22 +14,28 @@ const Header = () => {
   
     <div className='container container-header'>
     
-        <div className='row'>
-            <div className='col'>
-                <h1>Tech Stacks</h1>
+        <div className='main-info'>
+                <h1>Web Developer</h1>
+                <Typed className='typed-text'
+                strings={["Front-End Development", "Responsive web design", "UX Design"]}
+                typeSpeed={70}
+                backSpeed={30}
+                loop
+               />
                 <div className="tech-logos">
                  <img src={tlogo1}></img>
                  <img src={tlogo2}></img>
                  <img src={tlogo3}></img>
                  <img src={tlogo4}></img>
                 </div>
+
+                <a href="#" className='btn-main'>Contact Me</a>
                 <div>
                   
                 </div>
-            </div>
-            <div className='col'>
-                <h1>Front-End Developer</h1>
-            </div>
+          
+              
+      
         </div>
     </div>
   )
