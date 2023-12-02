c16841c592e9e7709192623186569e69b66c74b0
hizki-rp <hizkielas@gmail.com>
2023-12-02 21:54:27 +0300

    app fix

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import './App.css';
 
 import NavBar from './Components/NavBar';
 import Header from './Components/Header';
+import About from './Components/About';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+  return (
+     
+      <>
+         
+         <NavBar/>
+         <Header/>
+         <About/>
+         
+      </>
+          
+  );
+}
+
+export default App;
