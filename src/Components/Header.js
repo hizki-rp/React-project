b5afdd43a0ad89f0ecf7dd911f699ec86daa4e45
hizki-rp <hizkielas@gmail.com>
2023-12-19 19:43:46 +0300

    footer

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,4 @@
 import React from 'react';
-import tlogo1 from '../assets/javascript.png';
-import tlogo2 from '../assets/react.png';
-import tlogo3 from '../assets/bootstrap.png';
-import tlogo4 from '../assets/github.png';
-
 
 import Typed from 'react-typed';
 
